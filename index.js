e725b87028d56671b2c4658cfa8d004751396fb2
Roopa <roopavidhyaa@gmail.com>
2021-10-19 01:25:43 +0530

    Add Projects section

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
 function scrollDown(){
     console.log("Onclick")
-    document.getElementById("education").scrollIntoView();
+    document.getElementById("projects").scrollIntoView();
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("project-card");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
 }
